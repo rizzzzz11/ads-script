cd23a80e43c1f80c18c9ca0fad1b1d856a3c390d
rizzzzz11 <maulanawahyurizky@gmail.com>
2026-03-29 18:26:55 +0700

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,10 @@
+<script>
+  atOptions = {
+    'key' : 'f7bfcd98d0e442068ce7c183fc4ae266',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/f7bfcd98d0e442068ce7c183fc4ae266/invoke.js"></script>
